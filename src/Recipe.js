56a15601239dd6cc7f27910f8059fc049052dfb4
Ariela <arielaisrael12@gmail.com>
2023-09-15 01:49:36 -0400

    data added to gain functionality

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,42 @@
 import React from 'react';
 
-function Recipe() {
+function Recipe({
+	name,
+	cuisine,
+	photo,
+	ingredients,
+	preparation,
+	deleteBtnHandler,
+}) {
 	return (
-		<>
-			<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr>
-		</>
+		<tr>
+			<td>
+				<p>{name}</p>
+			</td>
+			<td>
+				<p>{cuisine}</p>
+			</td>
+			<td>
+				<img
+					src={photo}
+					alt=''
+				/>
+			</td>
+			<td className='content_td'>
+				<p>{ingredients}</p>
+			</td>
+			<td className='content_td'>
+				<p>{preparation}</p>
+			</td>
+			<td>
+				<button
+					name='delete'
+					onClick={deleteBtnHandler}
+				>
+					Delete
+				</button>
+			</td>
+		</tr>
 	);
 }
 
