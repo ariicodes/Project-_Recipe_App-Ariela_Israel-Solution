e72a94809b7beb8e700a8acae4755a4ec9d298a8
Ariela <arielaisrael12@gmail.com>
2023-09-14 22:02:14 -0400

    initial commit

---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Recipe() {
+	return (
+		<>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</>
+	);
+}
+
+export default Recipe;
